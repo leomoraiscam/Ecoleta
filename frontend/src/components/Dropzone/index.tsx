58fbcddc340a1f5046119c826972fa3719f3e12a
leomoraiscam <leozonnn@gmail.com>
2020-09-30 18:55:34 -0300

    subindo o frontend

---
+++ frontend/src/components/Dropzone/index.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { FiUpload } from "react-icons/fi";
+import "./styles.css";
+
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState("");
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      const fileUrl = URL.createObjectURL(file);
+
+      setSelectedFileUrl(fileUrl);
+      onFileUploaded(file);
+    },
+    [onFileUploaded]
+  );
+
+  const { getInputProps, getRootProps } = useDropzone({
+    onDrop,
+    accept: "image/*",
+  });
+
+  return (
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept="image/*" />
+
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} alt="imagem do estabelecimento" />
+      ) : (
+        <p>
+          <FiUpload />
+          Imagem do estalebecimento
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Dropzone;
